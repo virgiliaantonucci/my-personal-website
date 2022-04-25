32365d465a30cde6b200cb7a367214f918929b45
Virgilia Antonucci <virgilia.antonucci@gmail.com>
2022-04-24 22:57:49 -0400

    my personal website

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "I'm currently learning software development at Flatiron School. I will add more as I learn more.";
 document.querySelector("body").appendChild(h2);
